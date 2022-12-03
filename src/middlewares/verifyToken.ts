e6185001b67b7e426ec74914d2481df4042d4f49
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-03 02:38:51 -0300

    verify token middleware

---
+++ src/middlewares/verifyToken.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+function verifyToken(req: Request, res: Response, next: NextFunction) {
+	const getToken = req.headers["authorization"];
+	const token = getToken?.replace("Bearer ", "");
+
+	if (!token) {
+		throw {
+			code: "Unauthorized",
+			message: "A token is required for authentication",
+		};
+	}
+
+	const SECRET: string = process.env.TOKEN_SECRET_KEY ?? '';
+	
+		const decoded = jwt.verify(token, SECRET);
+
+		res.locals.tokenDecoded = decoded;
+
+		next();
+	
+}
+
+export default verifyToken;
