88a6508c3e8cd051a476f044f64a9338a68360c6
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-03 03:39:40 -0300

    auth service

---
+++ src/services/authService.ts
@@ -0,0 +1,60 @@
+// import * as validationService from "./validationService"
+import * as authRepository from "../repositories/authRepository"
+import { generateToken } from "../utils/generateToken"
+import { EncryptData }  from "../utils/bcrypt"
+import { TypeUser, TypeLogin} from "../utils/interfaces"
+import bcrypt from "bcrypt"
+
+export async function signUp(
+    userData: TypeUser){
+
+    await validateToSignUp(userData)
+    
+    const {password} = userData
+    const encryptedPassword : string = EncryptData(password)
+
+    await authRepository.createUser({...userData, password:encryptedPassword}) 
+}
+
+export async function signIn(
+    userData: TypeLogin){
+
+    const {id} = await validateToSignIn(userData)
+    const token = generateToken(id)
+    return token
+}
+
+export async function validateToSignUp(
+    userData: TypeUser
+){
+    const {email} = userData
+    const user =  await authRepository.searchUserByEmail(email)
+
+    if(user){
+        throw { code: "Unauthorized", message: "E-mail already registered"}
+    }
+   
+}
+
+export async function validateToSignIn(
+    userData: TypeLogin){
+
+    const {email, password} = userData
+    const user =  await authRepository.searchUserByEmail(email)      
+
+    if(!user){
+        throw { code: "Not Found", message: "Invalid e-mail "}
+    }
+
+    await validatePassword(password, user.password)    
+    return user
+}
+
+export async function validatePassword(
+    password: string,
+    encryptedPassword: string){
+
+    if(!bcrypt.compareSync(password, encryptedPassword)){
+        throw { code: "Not Found", message: "Invalid password"}
+    }
+}
