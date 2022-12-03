c2f711bf697ac875cb0ef4f7c942344684d827c4
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-03 05:21:56 -0300

    records controller

---
+++ src/controllers/recordsController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express"
+import * as recordsService from "../services/recordsService"
+import { TypeRecord } from "../utils/interfaces"
+
+export async function createRecord(req: Request, res: Response){
+        
+    const recordData: TypeRecord = req.body 
+    await recordsService.createRecords(recordData)
+    res.send("Registration successfully complete").status(201)    
+}
+
+export async function getRecords(req: Request, res: Response){
+
+    const userId : number = Number(req.params.userId)
+
+    const records = await recordsService.getRecords(userId)
+    res.send(records).status(200)
+}
