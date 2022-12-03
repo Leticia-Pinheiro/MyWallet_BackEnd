4e73d65527626fd012d4943667ae6248df3fa125
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-03 03:29:23 -0300

    auth controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express"
+import * as authService from "../services/authService"
+import { TypeUser, TypeLogin } from "../utils/interfaces"
+
+export async function signUp(req: Request, res: Response){
+        
+    const userData: TypeUser = req.body 
+    await authService.signUp(userData)
+    res.send("Registration successfully complete").status(201)    
+}
+
+export async function signIn(req: Request, res: Response){
+
+    const userData : TypeLogin = req.body 
+    const token = await authService.signIn(userData)
+    res.send(token).status(200)
+}
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -2,4 +2,6 @@ import {users, records} from "@prisma/client"
 
 export type TypeUser = Omit<users, 'id'>
 
+export type TypeLogin = Omit<users, 'id' | 'name'>
+
 export type TypeRecord = Omit<records, 'id'>
