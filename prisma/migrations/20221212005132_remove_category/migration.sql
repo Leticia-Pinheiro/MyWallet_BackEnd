00876cc378f840b00d4c317a49d361e223b4d4d5
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-11 21:54:31 -0300

    remove category

---
+++ prisma/migrations/20221212005132_remove_category/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `category` on the `records` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "records" DROP COLUMN "category";
--- src/schemas/recordSchema.ts
+++ src/schemas/recordSchema.ts
@@ -8,8 +8,7 @@ const recordSchema = joi.object({
 	value: joi.number().required(),
     description: joi.string().required(),
     date: joi.date().format('DD/MM').required(),
-    type: joi.valid('incoming', 'outgoing').required(),
-    category: joi.string().required()
+    type: joi.valid('incoming', 'outgoing').required()    
 });
 
 export default recordSchema;
